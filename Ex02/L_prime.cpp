90cf422f0f688f899ca99683237c386602130bfc
Luma41 <55790711+Luma41@users.noreply.github.com>
2021-04-12 22:40:39 +0200

    Update L_prime.cpp

--- Ex02/L_prime.cpp
+++ Ex02/L_prime.cpp
@@ -11,6 +11,12 @@ int main() {
     cout << "Please enter a number: ";
     int num;
     cin >> num;
+    int input;
+
+    cout << "What do you want to do? please enter a number" << endl;
+    cout << "(1) Sub.) a)" << endl;
+    cout << "(>2) Sub.) b) " << endl;
+    cin >> input;
 
     /* Start the time measurement */
     chrono::time_point<clock_> start = clock_::now();
@@ -19,7 +25,34 @@ int main() {
 
     /*
     Implement the prime check here
-    */
+   
+   a) */
+    if (input == 1) {
+        int i = 2;
+        while (i < num) {
+            if (num % i != 0) {
+                isPrime = true;
+                break;
+            }
+            else {
+                isPrime = false;
+            }
+
+            i++;
+        }
+    }
+
+    //b) alle Zahlen können mit den Primzahlen < 10 dargestellt werden
+    else {
+        if (num == 2 || num == 3 || num == 5 || num == 7) {
+            isPrime = true;
+        }
+        else {
+            if (!(num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0)) {
+                isPrime = true;
+            }
+        }
+    }
 
     cout << "The number " << num << " is a prime?: " << isPrime << endl;
     /* Stop time measurement and output the duration */
@@ -27,7 +60,6 @@ int main() {
         (clock_::now() - start).count() << "s" << endl << endl;
 
     /*
-   c) 100000004987 is a prime
+    c) 100000004987 is a prime
     */
-  
 }
