7e3ec90256ecde5527456a1ba890c788fd5c9b79
Sean <Sean@DESKTOP-9JUULL6>
2021-04-12 11:52:29 +0200

    indent

--- Ex01/Sean/1.3_midnightequation.cpp
+++ Ex01/Sean/1.3_midnightequation.cpp
@@ -26,8 +26,8 @@ int main()
     cout << "Enter a number for c: ";
     cin >> c;
 
-        cout << "Your result is: " << endl;
-        cout << "x_1 = " << ((-1 * b) + sqrt(b * b - 4 * a * c) / (2 * a)) << endl;
-        cout << "x_2 = " << ((-1 * b) - sqrt(b * b - 4 * a * c) / (2 * a)) << endl;
+    cout << "Your result is: " << endl;
+    cout << "x_1 = " << ((-1 * b) + sqrt(b * b - 4 * a * c) / (2 * a)) << endl;
+    cout << "x_2 = " << ((-1 * b) - sqrt(b * b - 4 * a * c) / (2 * a)) << endl;
 
 };
