9bc29dac93b9d826cdaad1ebd19dd980f010430f
Sean <Sean@DESKTOP-9JUULL6>
2021-04-12 11:44:13 +0200

    fix errors

--- Ex01/JPKirchner_main.cpp
+++ Ex01/JPKirchner_main.cpp
@@ -70,6 +70,15 @@ static void mitternachtsFormel(int a, int b, int c) {
 
 static int ggT(int a, int b) {
 
+	if (a < 0)
+	{
+		a = a * -1;
+	}
+	if (b < 0)
+	{
+		b = b * -1;
+	}
+
 	if (a == b) {
 	
 		return (a);
