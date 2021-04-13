994b2b7fc3725fbf85ef474b030ab0ee36224688
Sean <Sean@DESKTOP-9JUULL6>
2021-04-13 18:25:05 +0200

    add comment

--- Ex03/3.2/Sean_schleifen_und_summen_von_folgen_c.cpp
+++ Ex03/3.2/Sean_schleifen_und_summen_von_folgen_c.cpp
@@ -10,8 +10,42 @@ int main() {
 	unsigned long int max_start = 0;
 	
 	// Your code here
+	unsigned long int n = 1;
+	const double END = pow(10, 6);
 
+	// FIXME: Shit's not running...
 
+	while (n <= END) {
+
+		bool isConverged = false;
+		long int one = 0;
+		long int two = 0;
+		long int three = 0;
+	
+		while (!isConverged) {
+
+			if (one == 4 && two == 2 && three == 1) {
+				 
+				//n = 0;
+				isConverged = true;
+			}
+
+			if (n % 2 == 0) {
+				n = n / 2;
+			}
+			else {
+				n = (3 * n) + 1;
+			}
+
+			one = two;
+			two = three;
+			three = n;
+			
+		}
+
+		std::cout << n << std::endl;
+		n++;
+	}
 
 	std::cout << "The Collatz conjecture is true for the first 10^6 start values." << std::endl;
 	std::cout << "The longest sequences is created with n = " << max_start
