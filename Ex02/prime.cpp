8780afef94149389b52adcbae91c940ea50a98fd
Luma41 <55790711+Luma41@users.noreply.github.com>
2021-04-13 21:51:49 +0200

    Update prime.cpp

--- Ex02/prime.cpp
+++ Ex02/prime.cpp
@@ -1,73 +1,82 @@
 #include <iostream>
 /* Only used for time measurements */
 #include <chrono>
-#include <cmath>
 typedef std::chrono::high_resolution_clock clock_;
 typedef std::chrono::duration<double, std::ratio<1> > second_;
 
 using namespace std;
 
-
-
 int main() {
-	/* Read in the potential prime */
-	cout << "Please enter a number: ";
-	unsigned long long num;
-	cin >> num;
+    /* Read in the potential prime */
+    cout << "Please enter a number: ";
+    unsigned long long num;
+    cin >> num;
 
-	/* Start the time measurement */
-	chrono::time_point<clock_> start = clock_::now();
+    /* Start the time measurement */
+    chrono::time_point<clock_> start = clock_::now();
 
-	bool isPrime = true;
+    bool isPrime = false;
 
-	if (num <= 1) {
-		isPrime = false;
-	}
+    /*
+    Implement the prime check here
+    */
 
-	for (unsigned long long i = 2; i < num; i++) {
-		if (num % i == 0) {
-			isPrime = false;
+    if(num == 2) {
+        isPrime = true;
+    }
+    else if(num > 2) {
+        isPrime = true;
+        for(unsigned long long i = 2; i < num; i++) {
+            if(num % i == 0) {
+                cout << "found divisor: " << i << endl;
+                isPrime = false;
+            }
+        }
+    }
 
-		}
-	}
+    cout << "The number " << num << " is a prime?: " << isPrime << endl;
+    cout << "Calculation took: " << chrono::duration_cast<second_>
+            (clock_::now() - start).count() << "s"  << endl << endl;
 
-	cout << "The number " << num << " is a prime?: " << isPrime << endl;
-	/* Stop time measurement and output the duration */
-	cout << "Calculation took: " << chrono::duration_cast<second_>
-		(clock_::now() - start).count() << "s" << endl << endl;
+    /*
+    Remove the block comment below
+    */
+
+    cout << "Optimized version" << endl << endl;
+    start = clock_::now();
+
+    isPrime = false;
+
+    if(num == 2)
+        isPrime = true;
+    else if(num > 2 && num % 2 != 0) {
+        isPrime = true;
+        for (unsigned long long i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) {
+                cout << "found divisor: " << i << endl;
+                isPrime = false;
+                break;
+            }
+        }
+    }
 
 
-	cout << "Optimized version" << endl << endl;
-	start = clock_::now();
-
-	isPrime = true;
-
-	if (num <= 1) {
-		isPrime = false;
-	}
-
-	for (unsigned long long i = 2; i < sqrt(num)+1; i++) {
-		if (num % i == 0) {
-			isPrime = false;
-			break;
-
-		}
-	}
-
-
-	cout << "The number " << num << " is a prime?: " << isPrime << endl;
-	cout << "Calculation took: " << chrono::duration_cast<second_>
-		(clock_::now() - start).count() << "s" << endl << endl;
-
+    cout << "The number " << num << " is a prime?: " << isPrime << endl;
+    cout << "Calculation took: " << chrono::duration_cast<second_>
+            (clock_::now() - start).count() << "s"  << endl << endl;
 }
 
 /*
-Please enter a number: 100000004987
-The number 100000004987 is a prime?: 1
-Calculation took: 1142.59s
-
-Optimized version
-
-The number 100000004987 is a prime?: 1
-Calculation took: 0.0129457s
+ * ==================
+ * Results
+ * ==================
+ *
+ * Trivial Version:
+ * The number 100000004987 is a prime?: 1
+ * Calculation took: 485.119s
+ *
+ *
+ * Optimized version
+ * The number 100000004987 is a prime?: 1
+ * Calculation took: 0.0009679s
  */
