ec58ea6d553a55a3d9dad466cd692b6a9cb26dec
Sean <Sean@DESKTOP-9JUULL6>
2021-04-13 13:13:41 +0200

    fix samll error in 3c)i

--- Ex03/unerwartet_2.cpp
+++ Ex03/unerwartet_2.cpp
@@ -5,4 +5,14 @@ int main() {
 		i = 3;
 	}
 	std::cout << i << std::endl;
+
+
+	int index = 8;
+	int sum = 0;
+	while (index >= 4) {
+		index -= 2;
+		sum += index;
+	}
+	std::cout << sum << std::endl;
+
 }
