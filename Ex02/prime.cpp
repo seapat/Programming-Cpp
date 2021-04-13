6f38984a15b7c7a76e8d3fe79a17c3a45883b577
JPKirchner <38767556+JPKirchner@users.noreply.github.com>
2021-04-13 12:33:34 +0200

    Enddatei

---
+++ Ex02/prime.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+/* Only used for time measurements */
+#include <chrono>
+#include <cmath>
+typedef std::chrono::high_resolution_clock clock_;
+typedef std::chrono::duration<double, std::ratio<1> > second_;
+
+using namespace std;
+
+
+
+int main() {
+	/* Read in the potential prime */
+	cout << "Please enter a number: ";
+	unsigned long long num;
+	cin >> num;
+
+	/* Start the time measurement */
+	chrono::time_point<clock_> start = clock_::now();
+
+	bool isPrime = true;
+
+	if (num <= 1) {
+		isPrime = false;
+	}
+
+	for (unsigned long long i = 2; i < num; i++) {
+		if (num % i == 0) {
+			isPrime = false;
+
+		}
+	}
+
+	cout << "The number " << num << " is a prime?: " << isPrime << endl;
+	/* Stop time measurement and output the duration */
+	cout << "Calculation took: " << chrono::duration_cast<second_>
+		(clock_::now() - start).count() << "s" << endl << endl;
+
+
+	cout << "Optimized version" << endl << endl;
+	start = clock_::now();
+
+	isPrime = true;
+
+	if (num <= 1) {
+		isPrime = false;
+	}
+
+	for (unsigned long long i = 2; i < sqrt(num+1); i++) {
+		if (num % i == 0) {
+			isPrime = false;
+			break;
+
+		}
+	}
+
+
+	cout << "The number " << num << " is a prime?: " << isPrime << endl;
+	cout << "Calculation took: " << chrono::duration_cast<second_>
+		(clock_::now() - start).count() << "s" << endl << endl;
+
+}
+
+/*
+Please enter a number: 100000004987
+The number 100000004987 is a prime?: 1
+Calculation took: 1142.59s
+
+Optimized version
+
+The number 100000004987 is a prime?: 1
+Calculation took: 0.0129457s
+ */
