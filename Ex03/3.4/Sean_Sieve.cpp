fd338dfc987f089d85f1d2fcd6dd836492f79dc8
Sean <Sean@DESKTOP-9JUULL6>
2021-04-14 10:39:13 +0200

    solution 3.4

--- Ex03/3.4/Sean_Sieve.cpp
+++ Ex03/3.4/Sean_Sieve.cpp
@@ -7,23 +7,21 @@ int main() {
 
     //unsigned int k = 2;
     unsigned long int n = 2;
-    
-    
-	cout << "Enter a maximum: ";
-	cin >> n;
-	cout << endl;
-    
+
+
+    cout << "Enter a maximum: ";
+    cin >> n;
+    cout << endl;
+
 
     // initialize array: all naturals are primes
-
-    //size_t size = n + 1;
-    bool* primes = new bool[n+1]{ false, false };
+    bool* primes = new bool[n + 1]{ false, false };
     for (long int i = 2; i < n; i++)
     {
         primes[i] = true;
     }
- 
-    // iteratively mark non-primes
+
+    // iteratively mark non-primes as false
     for (long int k = 2; k * k <= n; k++)
     {
         if (primes[k] == true)
@@ -34,11 +32,11 @@ int main() {
         }
     }
 
-    /* 
+    /*
     for (int i = 0; i < n; i++) {
         cout << primes[i] << "  ";
     }
-    */
+    
     cout << endl;
     for (int i = 0; i < n; i++) {
         if (primes[i] == 1) {
@@ -48,11 +46,16 @@ int main() {
             cout << "";
         }
     }
+    */
+
+
+    //build sum from indices of all primes ( == true )
+    long int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (primes[i] == true) {
+            sum += i;
+        }
+    }
+    cout << "sum: " << sum;
     cout << endl << "done";
-    
-
-
-    //build sum
-
-
 }
