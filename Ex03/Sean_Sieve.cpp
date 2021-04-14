be5ad83921dadab32aee416578f2c2d3b176422d
Sean <Sean@DESKTOP-9JUULL6>
2021-04-14 10:29:57 +0200

    properly getting primes

--- Ex03/Sean_Sieve.cpp
+++ Ex03/Sean_Sieve.cpp
@@ -1,19 +1,58 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
 int main() {
 
-	int k = 2;
-	int n = k;
-	cout << "Enter a maximum: "
+    //unsigned int k = 2;
+    unsigned long int n = 2;
+    
+    
+	cout << "Enter a maximum: ";
 	cin >> n;
 	cout << endl;
+    
 
-	int arr[n - k] = {};
+    // initialize array: all naturals are primes
 
-	for (int i = 0; i < sizeof(arr); i++) {
-		arr[i] = i + 1;
-	}
+    //size_t size = n + 1;
+    bool* primes = new bool[n+1]{ false, false };
+    for (long int i = 2; i < n; i++)
+    {
+        primes[i] = true;
+    }
+ 
+    // iteratively mark non-primes
+    for (long int k = 2; k * k <= n; k++)
+    {
+        if (primes[k] == true)
+            //cout << k;
+        {
+            for (long int j = k * 2; j <= n; j += k)
+                primes[j] = false;
+        }
+    }
+
+    /* 
+    for (int i = 0; i < n; i++) {
+        cout << primes[i] << "  ";
+    }
+    */
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        if (primes[i] == 1) {
+            cout << i << "  ";
+        }
+        else {
+            cout << "";
+        }
+    }
+    cout << endl << "done";
+    
+
+
+    //build sum
+
 
 }
